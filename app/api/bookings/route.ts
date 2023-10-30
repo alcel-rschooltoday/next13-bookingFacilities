f7cf4ea052d0517f392f0060713c796e9c109899
alcel <alcel@rschooltoday.com>
2023-10-30 11:44:27 +0800

    Minor fixes

--- app/api/bookings/route.ts
+++ app/api/bookings/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { db } from "@vercel/postgres";
 
-export default async function handler(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const client = await db.connect();
   try {
     await client.sql`CREATE TABLE IF NOT EXISTS booking ( 
