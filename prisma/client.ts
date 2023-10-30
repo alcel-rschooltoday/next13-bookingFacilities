9cea54971296650ff65d403065a17696bffc873a
alcel <alcel@rschooltoday.com>
2023-10-30 17:21:57 +0800

    postgres deployment

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,14 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.POSTGRES_URL,
-      },
-    },
-  });
-};
+  return new PrismaClient();
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
