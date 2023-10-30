67656117d9783d2c9df2b5ed84e8ae5bfdb4fa55
alcel <alcel@rschooltoday.com>
2023-10-30 20:38:23 +0800

    Make changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
-        My app
+        Booking Facilities
       </Link>
       <Link className="bg-white p-2" href={"/createBooking"}>
         Create Booking
